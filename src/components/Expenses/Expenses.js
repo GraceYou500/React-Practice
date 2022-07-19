8c238eb0bbbb2ec536a7ca64695aedf305fc5b9f
Grace You <graceyouya500@gmail.com>
2022-07-19 18:14:41 +1000

    assignement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,47 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../GeneralUI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
 function Expenses(props) {
+  const [filteredYearData, setFilteredYearData] = useState('2020');
+
+  const filteredYearHandler = selectedYear => {
+    setFilteredYearData(selectedYear);
+    console.log('here2', selectedYear);
+  };
+  console.log('here3', filteredYearData);
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
+    <div>
+      <ExpensesFilter
+        onFilteredYear={filteredYearHandler}
+        filteredYear={filteredYearData}
       />
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      />
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      />
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      />
-    </Card>
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.item[0].title}
+          amount={props.item[0].amount}
+          date={props.item[0].date}
+        />
+        <ExpenseItem
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        />
+        <ExpenseItem
+          title={props.item[2].title}
+          amount={props.item[2].amount}
+          date={props.item[2].date}
+        />
+        <ExpenseItem
+          title={props.item[3].title}
+          amount={props.item[3].amount}
+          date={props.item[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 

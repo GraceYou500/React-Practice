dd3e1d34001c4b3fd71cb13cdc89f932692b9496
Grace You <graceyouya500@gmail.com>
2022-07-20 22:26:31 +1000

    finish chartbar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../GeneralUI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 function Expenses(props) {
   console.log(props);
@@ -23,6 +24,7 @@ function Expenses(props) {
           onFilteredYear={filteredYearHandler}
           filteredYear={filteredYearData}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
